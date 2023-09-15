d2690c156b59e213d34417af43560b99b23199cc
Sakil Hasan Saikat <72136146+0xSaikat@users.noreply.github.com>
2023-09-15 19:21:03 +0600

    Create main.py

---
+++ Unpack a Collection/main.py
@@ -0,0 +1,11 @@
+print("B+ blood group members")
+
+Blood = ["Saikat-123344", "Sojib-122333", "Nazia-1111123", "Rafia-12222"]
+x, y, z, a = Blood
+
+print(x)
+print(y)
+print(z)
+print(a)
+
+print("Thank you and see you soon!")
