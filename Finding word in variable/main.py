322505c16c0b2d234404831e4e1b0e1ce9475a31
Sakil Hasan Saikat <72136146+0xSaikat@users.noreply.github.com>
2023-09-16 09:56:20 +0600

    Create word finding from text code main.py

---
+++ Finding word in variable/main.py
@@ -0,0 +1,8 @@
+print("Is Bangladesh word on the text?")
+
+text = "Northan University Bangladesh"
+
+print("Bangladesh" in text)
+
+if "Bangladesh" in text:
+    print("Yes, Bangladesh on the text!")
